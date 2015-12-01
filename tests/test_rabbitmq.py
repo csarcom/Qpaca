5f38319bcc2cad3f7d8ea1985379b51e89e32f44
Charles Sartori <charles.sartori@gmail.com>
2015-12-01 21:55:20 -0200

    Add tests for RabbitMQ backend
    We are testing here RabbitMQ Backend and RabbitMQ publisher class.
    I'm not sure yet how to implement Subscriber class

---
+++ tests/test_rabbitmq.py
@@ -0,0 +1,54 @@
+import mock
+
+
+from pubsub.backend.rabbitmq import RabbitMQ, RabbitMQPublisher
+
+
+class TestRabbitMQ(object):
+    def setup_class(self):
+        self.backend = RabbitMQ()
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher.start')
+    def test_call_start_publisher(self, mocked_function):
+        self.backend.start()
+        assert mocked_function.called
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber.start')
+    def test_call_start_subscriber(self, mocked_function):
+        self.backend.start()
+        assert mocked_function.called
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher.publish')
+    def test_call_publish(self, mocked_function):
+        self.backend.publish(None)
+        assert mocked_function.called
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher.publish')
+    def test_publisher_publish_args(self, mocked_function):
+        self.backend.publish('message')
+        mocked_function.assert_called_with('message')
+
+
+class TestRabbitMQPublisher(object):
+    def setup_class(self):
+        self.publisher = RabbitMQPublisher()
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._connect')
+    def test_call_connect(self, mocked_function):
+        self.publisher.start()
+        assert mocked_function.called
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._create_exchange')
+    def test_call_create_exchange(self, mocked_function):
+        self.publisher.start()
+        assert mocked_function.called
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._create_producer')
+    def test_call_create_producer(self, mocked_function):
+        self.publisher.start()
+        assert self.publisher._create_producer.called
+
+    def test_call_producer_publish(self):
+        self.publisher._producer = mock.Mock()
+        self.publisher.publish(None)
+        assert self.publisher._producer.publish.called
