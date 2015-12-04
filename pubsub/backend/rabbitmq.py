7c3db5747bd22e91fae76e88643ee26f544c5b34
Charles Sartori <charles.sartori@gmail.com>
2015-12-04 21:48:27 -0200

    Add RabbitMQSubscriber using gevent
    Now we have a way to asynchronous consume new messages. it is using
    gevent for now, but, I intend to change that to py3.5 new async/await.

--- pubsub/backend/rabbitmq.py
+++ pubsub/backend/rabbitmq.py
@@ -1,6 +1,12 @@
+from gevent import monkey
+monkey.patch_all()
+
 import uuid
 
 import kombu
+import gevent
+
+from kombu.mixins import ConsumerMixin
 
 from pubsub.backend.base import BaseBackend
 
@@ -13,6 +19,7 @@ class RabbitMQ(BaseBackend):
     def start(self):
         self.publisher.start()
         self.subscriber.start()
+        gevent.spawn(self.subscriber.run_forever)
 
     def publish(self, message):
         return self.publisher.publish(message)
@@ -23,20 +30,22 @@ class RabbitMQPublisher(object):
         self.exchange_name = 'python-pubsub'
         self.amqp_address = 'amqp://guest:guest@my_rabbitmq:5672//'
         self.routing_key = 'pubsub'
+        self._connection = self._connect()
 
     def start(self):
-        self._connection = self._connect()
         self._exchange = self._create_exchange()
         self._producer = self._create_producer()
 
     def publish(self, message):
         message_id = str(uuid.uuid4())
         message = {'payload': message,
-                   'uuid': message_id}
+                   'message_id': message_id,
+                   'reply_to': None}
 
         self._producer.publish(
             message, exchange=self._exchange, routing_key=self.routing_key,
             serializer='json', compression='zlib', retry=True)
+        print ('Message sent')
         return message_id
 
     def _create_producer(self):
@@ -55,12 +64,52 @@ class RabbitMQPublisher(object):
             self.amqp_address,
             transport_options={'confirm_publish': True},
             failover_strategy='round-robin')
-        return connection.ensure_connection()
+        connection.ensure_connection()
+        return connection
 
 
-class RabbitMQSubscriber(object):
+class RabbitMQSubscriber(ConsumerMixin):
     def __init__(self, *args, **kwargs):
-        pass
+        self.exchange_name = 'python-pubsub'
+        self.amqp_address = 'amqp://guest:guest@my_rabbitmq:5672//'
+        self.routing_key = 'pubsub'
+        self.queue_name = 'store'
+        self.connection = self._connect()
 
     def start(self):
-        pass
+        self._exchange = self._create_exchange()
+        self._queue = self._create_queue()
+
+    def run_forever(self):
+        self.run()
+
+    def get_consumers(self, consumer, channel):
+        return [consumer(
+            queues=[self._queue],
+            callbacks=[self.on_message],
+            accept={'application/json'},)]
+
+    def _connect(self):
+        connection = kombu.Connection(self.amqp_address,
+                                      failover_strategy='round-robin')
+        connection.ensure_connection()
+        return connection
+
+    def _create_exchange(self):
+        exchange = kombu.Exchange(
+            self.exchange_name,
+            type='direct',
+            durable=True, auto_delete=False)(self.connection)
+        exchange.declare()
+        return exchange
+
+    def _create_queue(self):
+        queue = kombu.Queue(
+            self.queue_name, self._exchange,
+            routing_key=self.routing_key, auto_delete=True)(self.connection)
+        queue.declare()
+        return queue
+
+    def on_message(self, body, message):
+        print ('Got message: {0}'.format(body))
+        message.ack()
