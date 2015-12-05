ce8d0d1c2a860fea2d9a2119ec41d630c2e23df6
Charles Sartori <charles.sartori@gmail.com>
2015-12-05 10:28:13 -0200

    Fix tests for rabbitmq backend
    After #11 test was broken, this commit fix it .

--- tests/test_rabbitmq.py
+++ tests/test_rabbitmq.py
@@ -38,11 +38,12 @@ class TestRabbitMQPublisher(object):
 
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._connect')
     def setup_class(self, mock1):
-        self.publisher = RabbitMQPublisher()
+        config = mock.Mock()
+        self.publisher = RabbitMQPublisher(config=config)
 
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._connect')
     def test_call_connect(self, mocked_function):
-        RabbitMQPublisher()
+        RabbitMQPublisher(config=mock.Mock())
         assert mocked_function.called
 
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._create_exchange')
@@ -50,12 +51,14 @@ class TestRabbitMQPublisher(object):
         self.publisher.start()
         assert mocked_function.called
 
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._create_exchange')
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQPublisher._create_producer')
-    def test_call_create_producer(self, mocked_function):
+    def test_call_create_producer(self, mocked_exchange, mocked_producer):
         self.publisher.start()
         assert self.publisher._create_producer.called
 
     def test_call_producer_publish(self):
+        self.publisher.config = {'publish': {}}
         self.publisher._producer = mock.Mock()
         self.publisher.publish(None)
         assert self.publisher._producer.publish.called
@@ -65,23 +68,24 @@ class TestRabbitMQSubscriber(object):
 
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._connect')
     def setup_class(self, mock1):
-        self.subscriber = RabbitMQSubscriber()
+        self.subscriber = RabbitMQSubscriber(config=mock.Mock())
 
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._connect')
     def test_call_connect(self, mocked_function):
         RabbitMQSubscriber()
         assert mocked_function.called
 
-    @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._create_queue')
-    def test_call_create_queue(self, mocked_function):
-        self.subscriber.start()
-        assert mocked_function.called
-
-    @pytest.mark.skipif(True, reason='For some reason its is not working')
     @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._create_exchange')
-    def test_call_create_exchange(self, mocked_function):
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._create_queue')
+    def test_call_create_queue(self, mocked_exchange, mocked_queue):
         self.subscriber.start()
-        assert mocked_function.called
+        assert mocked_queue.called
+
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._create_exchange')
+    @mock.patch('pubsub.backend.rabbitmq.RabbitMQSubscriber._create_queue')
+    def test_call_create_exchange(self, mocked_exchange, mocked_queue):
+        self.subscriber.start()
+        assert mocked_exchange.called
 
     def test_ack_message_on_message(self):
         message = mock.Mock(ack=mock.Mock(return_value=True))
@@ -90,14 +94,9 @@ class TestRabbitMQSubscriber(object):
         assert message.ack.called
 
     def test_return_get_consumers(self):
+        self.subscriber.config = {'consumer': {}}
         self.subscriber._queue = mock.Mock()
         self.subscriber.on_message = mock.Mock()
         consumers = self.subscriber.get_consumers(mock.Mock(), mock.Mock())
         assert type(consumers) is list
         assert len(consumers) == 1
-
-
-class TestSomething(object):
-    def test_something(self):
-        backend = RabbitMQ()
-        assert backend is not None
