af14f70431ed37ff71eb525767d712e8532f3111
Charles Sartori <charles.sartori@gmail.com>
2015-12-08 21:13:53 -0200

    Split RabbitMQ Publisher and Subscriber

--- pubsub/backend/base.py
+++ pubsub/backend/base.py
@@ -1,6 +1,14 @@
-class BaseBackend(object):
+class BasePublisher(object):
     def start(self):
         raise NotImplementedError
 
     def publish(self):
+        raise NotImplementedError
+
+
+class BaseSubscriber(object):
+    def start(self):
+        raise NotImplementedError
+
+    def on_message(self):
         raise NotImplementedError
