559936985621371c323500d87c0ff3158376437d
Charles Sartori <charles.sartori@gmail.com>
2015-12-15 22:27:49 -0200

    Add service discovery

--- pubsub/backend/handlers.py
+++ pubsub/backend/handlers.py
@@ -1,4 +1,7 @@
 import kombu
+import socket
+import requests
+import json
 
 from pubsub.helpers import logger
 
@@ -22,16 +25,29 @@ class RabbitMQHandler(object):
         """Create a RabbitMQ connection"""
 
         logger.debug('Connecting with RabbitMQ')
-        connection = kombu.Connection(**self.config.get('connection'))
+
+        # TODO: Isolate this requests.get
+        #       Look for a better way instead using host ip(if any)
+        req = requests.get(
+            'http://192.168.99.100:8500/v1/catalog/service/rabbitmq-5672')
+        hosts = ['{0}:{1}'.format(i['Address'], i['ServicePort'])
+                 for i in json.loads(req.text)]
+
+        # TODO: Make it connect using all hosts, for now we are always
+        # using one
+        connection = kombu.Connection(
+            hostname='amqp://guest:guest@{0}//'.format(hosts[0]),
+            **self.config.get('connection'))
         connection.ensure_connection()
         return connection
 
     def _create_queue(self):
         """Create a RabbitMQ queue"""
 
+        queue_name = socket.gethostname()
         logger.debug('Creating RabbitMQ Queue')
         queue = kombu.Queue(
-            exchange=self._exchange,
-            **self.config.get('queue'))(self.connection)
+            exchange=self._exchange, name=queue_name,
+            auto_delete=True, **self.config.get('queue'))(self.connection)
         queue.declare()
         return queue
